13bc2398f3b1329533ec0653064cf479a58710dc
Jacob Humston <humstonjacob@gmail.com>
2023-03-26 15:20:20 -0400

    front page finished

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+/*
+
+    Programmed by LovelyJacob (Jacob Humston).
+    This code is open source, however I kindly ask for credit if used.
+    Repository: https://github.com/jacobhumston/my-website
+
+    Have a good one! :)
+
+*/
+
+function discordButton() {
+    const element = document.getElementById('discord-link');
+    const elementText = document.getElementById('discord-text');
+    let coolDown = false;
+    let originalText = elementText.innerText;
+    element.addEventListener('click', function () {
+        if (coolDown) return;
+        coolDown = true;
+        try {
+            navigator.clipboard.writeText(originalText);
+            elementText.innerText = 'Copied!';
+        } catch {
+            elementText.innerText = 'Failed to copy!';
+        }
+        setTimeout(function () {
+            elementText.innerText = originalText;
+            coolDown = false;
+        }, 3000);
+    });
+}
+
+function loaded() {
+    discordButton();
+}
+
+window.addEventListener('load', loaded);
--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,13 @@
+/*
+
+    Programmed by LovelyJacob (Jacob Humston).
+    This code is open source, however I kindly ask for credit if used.
+    Repository: https://github.com/jacobhumston/my-website
+
+    Have a good one! :)
+
+*/
+
 function updateTitle() {
     document.title = document.title + " | LovelyJacob's Portfolio";
 }
