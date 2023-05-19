5b032ddb1b0e198b377c8015f30cf0ee5e5188d8
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2023-05-19 18:17:45 +0000

    new game but needs a lot of work lol

---
+++ random-games/guess-the-number/main.js
@@ -0,0 +1,10 @@
+/*
+
+    Programmed by LovelyJacob (Jacob Humston).
+    This code is open source, however I kindly ask for credit if used.
+    Repository: https://github.com/jacobhumston/my-website
+
+    Have a good one! :)
+
+*/
+
